161a799b6c6d511f094618cabf4dc2d1a4fd231f
Muaz-Shoukat <muazdev01@gmail.com>
2023-08-21 09:32:54 +0500

    Added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileupload = require("express-fileupload");
@@ -8,6 +9,7 @@ const dotenv = require("dotenv");
 const errorMiddleware = require("./middleware/error");
 
 dotenv.config({ path: "backend/config/config.env" });
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
